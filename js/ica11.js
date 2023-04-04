4cee959982fdb3e52ead8f861e3a255d8ba12c50
ParkerRoth <paro6495@colorado.edu>
2023-04-04 11:17:29 -0600

    ica11

---
+++ js/ica11.js
@@ -0,0 +1,39 @@
+const button = document.querySelector("#js-new-quote");
+button.addEventListener('click',getQuote);
+
+const answerButton = document.querySelector("#js-tweet")
+answerButton.addEventListener('click', showAnswer)
+
+const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion"
+
+
+
+async function getQuote(){
+    try {
+        const response = await fetch(endpoint)
+        if (!response.ok){
+            throw Error(response.statusText)
+        }
+        const json = await response.json();
+        displayQuote(json.question);
+        showAnswer(json.answer);
+
+    }
+    catch (err){
+        console.log(err);
+        alert('Failed to fetch new trivia');
+    }
+}
+
+function displayQuote(quote){
+    const quoteText = document.querySelector("#js-quote-text");
+    quoteText.textContent = quote;
+}
+
+async function showAnswer(quote){
+    const answerText = document.querySelector("#js-answer-text");
+    answerText.textContent = quote;
+}
+
+getQuote();
+
