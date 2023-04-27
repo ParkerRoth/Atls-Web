f5d80557a74ed41c8afb1e5d1e08129815451388
ParkerRoth <paro6495@colorado.edu>
2023-04-27 13:06:44 -0600

    ///

--- js-final/js-final.js
+++ js-final/js-final.js
@@ -2,39 +2,52 @@ const rockBtn = document.querySelector('#rock');
 const paperBtn = document.querySelector('#paper');
 const scissorsBtn = document.querySelector('#scissors');
 const resultDiv = document.querySelector('.game-result');
+const winCount = document.querySelector('#win-count');
+const loseCount = document.querySelector('#lose-count');
+const tieCount = document.querySelector('#tie-count');
+
+let wins = 0;
+let losses = 0;
+let ties = 0;
 
 function getComputerChoice() {
-	const choices = ['rock', 'paper', 'scissors'];
-	const randomIndex = Math.floor(Math.random() * 3);
-	return choices[randomIndex];
+  const choices = ['rock', 'paper', 'scissors'];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
 }
 
 function play(playerChoice) {
-	const computerChoice = getComputerChoice();
-
-	if (playerChoice === computerChoice) {
-		resultDiv.textContent = "It's a tie!";
-	} else if (
-		(playerChoice === 'rock' && computerChoice === 'scissors') ||
-		(playerChoice === 'paper' && computerChoice === 'rock') ||
-		(playerChoice === 'scissors' && computerChoice === 'paper')
-	) {
-		resultDiv.textContent = 'You win!';
-	} else {
-		resultDiv.textContent = 'You lose!';
-	}
+  const computerChoice = getComputerChoice();
+  if (playerChoice === computerChoice) {
+    resultDiv.textContent = "It's a tie!";
+    ties++;
+  } else if (
+    (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    (playerChoice === 'paper' && computerChoice === 'rock') ||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    resultDiv.textContent = 'You win!';
+    wins++;
+  } else {
+    resultDiv.textContent = 'You lose!';
+    losses++;
+  }
+  
+  winCount.textContent = `Wins: ${wins}`;
+  loseCount.textContent = `Losses: ${losses}`;
+  tieCount.textContent = `Ties: ${ties}`;
 }
 
 rockBtn.addEventListener('click', () => {
-	play('rock');
+  play('rock');
 });
 
 paperBtn.addEventListener('click', () => {
-	play('paper');
+  play('paper');
 });
 
 scissorsBtn.addEventListener('click', () => {
-	play('scissors');
+  play('scissors');
 });
 
 
